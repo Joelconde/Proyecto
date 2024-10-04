37a9b263571037ef31b607f514793db6b17dc3de
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-04 19:32:31 +0200

    Cambios

---
+++ Valoraciones.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Izeta3;
+
+CREATE TABLE Musicos (
+    id_musico INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    genero VARCHAR(100),
+    biografia TEXT,
+    fecha_nacimiento DATE
+);
+
+CREATE TABLE Albumes (
+);
+
+CREATE TABLE 
+
+
