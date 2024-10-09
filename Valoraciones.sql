a5165d3f6dad1d76d545f975d35691217d79ad3a
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-09 16:17:18 +0200

    Cambios en el .sql

--- Valoraciones.sql
+++ Valoraciones.sql
@@ -4,7 +4,7 @@ USE valoraciones_db;
 
 CREATE TABLE valoraciones (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
     comentario TEXT NOT NULL,
     puntuacion INT(1) NOT NULL CHECK (puntuacion BETWEEN 1 AND 5),
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
