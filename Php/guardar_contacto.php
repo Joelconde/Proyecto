364f7be3c9faa8d2b2722304e55b42895a83d6c9
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-28 20:08:02 +0100

    He añadido en .php para que me salgan los errores que tengo y para conectar a phpmyadmin

---
+++ Php/guardar_contacto.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+// Conectar a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "valoraciones_db";
