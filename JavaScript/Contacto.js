f895d699219d58d88ca1b5de5295069333c0505e
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-28 19:16:33 +0100

    He puesto un comentario en js explicando como funcionara el programa

---
+++ JavaScript/Contacto.js
@@ -0,0 +1,6 @@
+document.querySelector('.contact form').addEventListener('submit', function(event) {
+/* Si es valido entonces todos los campos de contact 
+se podran enviar pero si es falso y dejas algun espacio
+en blanco te saldra en rojo, pero si rellena el campo te saldra de color gris. */
+    let isValid 
+}
