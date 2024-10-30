7a207203d3fc8921a9c7fb1b669f5ec4fec17943
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-30 16:02:59 +0100

    He cambiado un poco el code de php poniendo un comentario al codigo si se utiliza xampp

--- Php/guardar_contacto.php
+++ Php/guardar_contacto.php
@@ -9,29 +9,33 @@ $username = "root";
 $password = "";
 $dbname = "valoraciones_db";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " .$conn->connect_error);
 }
+echo "Conexion exitosa";
 
-if (isset($_POST['nombre']) && isset($_POST['correo']) && isset($_POST['mensaje'])) {
-// Recibir los datos de contactos y sacarlos para evitar errores.
-$nombre = $conn->real_escape_string($_POST['nombre']);
-$correo = $conn->real_escape_string($_POST['correo']);
-$mensaje = $conn->real_escape_string($_POST['mensaje']);
-
-// Insertar los datos en la base de datos
-$sql = "INSERT INTO contacto (nombre, correo, mensaje) VALUES ('$nombre','$correo','$mensaje')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Contacto guardado con éxito";
-} else {
-    echo "Error: " .$sql. "<br>" .$conn->error;
-}
-} else {
-    echo "Todos los campos son obligatorios.";
-}
+/*if (isset($_POST['nombre']) && isset($_POST['correo']) && isset($_POST['mensaje'])) {
+    // Recibir los datos de contactos y sacarlos para evitar errores.
+    $nombre = $conn->real_escape_string($_POST['nombre']);
+    $correo = $conn->real_escape_string($_POST['correo']);
+    $mensaje = $conn->real_escape_string($_POST['mensaje']);
+    
+    // Insertar los datos en la base de datos
+    $sql = "INSERT INTO contacto (nombre, correo, mensaje) VALUES ('$nombre','$correo','$mensaje')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Contacto guardado con éxito";
+    } else {
+        echo "Error: " .$sql. "<br>" .$conn->error;
+    }
+    } else {
+        echo "Todos los campos son obligatorios.";
+    }
+    
+    $conn->close();
+    ?>*/
 
 $conn->close();
 ?>
