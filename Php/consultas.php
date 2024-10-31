4c9f87874ffe2681dfacd133985a2e23e307c0de
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-31 15:27:35 +0100

    Hemos metido un nuevo .php que sirve analizar consultar y leer datos y insertarlos

---
+++ Php/consultas.php
@@ -0,0 +1,10 @@
+<?php
+include 'guardar_valoraciones.php';
+
+// Insertar datos 
+$sql = 'INSERT INTO valoraciones (nombre, comentario, puntuacion) VALUES ('Iker','tope','3')';
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado";
+} else {
+    echo "Error: " .$sql. "<br>" .$conn->error;
+}
