1deb5222a4a302dca6d9a0f0301a2386bf932af6
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-31 16:07:15 +0100

    Update

--- Php/guardar_valoraciones.php
+++ Php/guardar_valoraciones.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include 'conexion.php';
+
 // Conectar a la base de datos
 $servername = "localhost";
 $username = "root";
