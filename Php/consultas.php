8f7c1eda05c609d24e632a32210df88c988107d5
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-10-31 16:10:31 +0100

    update

--- Php/consultas.php
+++ Php/consultas.php
@@ -3,7 +3,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-include 'conexion.php';
+include '/opt/lampp/htdocs/Proyecto/Php/conexion.php';
 include 'guardar_valoraciones.php';
 
 // Insertar datos 
