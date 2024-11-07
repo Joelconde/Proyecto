04c66422f2080ab4c8e51d2710b1f8a781fd237d
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-11-07 15:26:18 +0100

    Updte

--- Php/guardar_valoraciones.php
+++ Php/guardar_valoraciones.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'conexion.php';
+//include 'conexion.php';
 
 //Conectar a la base de datos
 $servername = "127.0.0.1";
