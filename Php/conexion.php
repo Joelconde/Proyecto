ca6d1c9ac6897f65133c49f303df43316f8a7386
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-11-07 15:24:16 +0100

    Update

--- Php/conexion.php
+++ Php/conexion.php
@@ -3,7 +3,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "user";
 $password = "@Mvm2016";
 $dbname = "valoraciones_db";
