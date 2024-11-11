a81365e32a2f505310a4a771162700eddd0f7c18
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-11-11 19:21:10 +0100

    Update

--- JavaScript/Contacto.js
+++ JavaScript/Contacto.js
@@ -4,6 +4,7 @@ document.querySelector('.contact-form').addEventListener('submit',function(event
 se podran enviar pero si es falso y dejas algun espacio
 en blanco te saldra en rojo, pero si rellena el campo te saldra de color gris. */
     let isValid = true;
+    const form = this;
         const inputs = this.querySelectorAll('input[required], textarea[required]');
         inputs.forEach(input => {
             if (!input.value.trim()) {
