385d62e72b096ccd532ddf6467744ffe0868cd71
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2024-12-04 16:40:48 +0100

    Update y he creado una carpeta js

---
+++ js/gotop.js
@@ -0,0 +1,8 @@
+window.onscroll - function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    } else {
+        
+    }
+}
