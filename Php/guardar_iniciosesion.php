8c1a552a2c78a1e5cc987314f06d7ef865f2dfdc
joelrodriguezconde00@gmail.com <joelrodriguezconde00@gmail.com>
2025-01-15 16:43:15 +0100

    Cambios

---
+++ Php/guardar_iniciosesion.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+include 'conexion.php';
+
+// Conectar a la base de datos
+$servername = "127.0.0.1";
+$username = "izeta3php";
+$password = "Camello@33";
+$dbname = "valoraciones_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " .$conn->connect_error);
+}
+
+if (isset($_POST['usuario']) && isset($_POST['correo']) && isset($_POST['contraseña'])) {
+    // Recibir los datos de contactos y sacarlos para evitar errores.
+    $nombre = $conn->real_escape_string($_POST['usuario']);
+    $correo = $conn->real_escape_string($_POST['correo']);
+    $mensaje = $conn->real_escape_string($_POST['contraseña']);
+    
+    // Insertar los datos en la base de datos
+    $sql = "INSERT INTO inicio_sesion (usuario, correo, contraseña) VALUES ('$usuario','$correo','$contraseña')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Contacto guardado con éxito";
+    } else {
+        echo "Error: " .$sql. "<br>" .$conn->error;
+    }
+} else {
+    echo "Todos los campos son obligatorios.";
+}
+
+// Cerrar la conexión
+mysqli_close($conn);    
+?>
